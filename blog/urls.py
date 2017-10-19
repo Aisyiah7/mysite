bc1ff78cae25615833655526b4e7a9c00d56f530
Aisyiah7 <nurulaisyiah.72@gmail.com>
2017-10-19 11:56:25 +0800

    create login  logout URLs

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    #Login/logout urls
+    url(r'^login/$','django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url(r'^logout/$','django.contrib.autj.views.logout', {'next_page': '/login/'}),
     url(r'^$', views.index, name='index'),
 ]
